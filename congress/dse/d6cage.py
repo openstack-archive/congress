1317d7b9d0ad55d7438f04d025533d5dd4b97f99
Harrison kelly <harrison.kelly@plexxi.com>
2014-04-18 14:51:22 -0400

    Initial commit
    Change-Id: If3d7506b116027b0d570187aac639c5cf7166302

---
+++ congress/dse/d6cage.py
@@ -0,0 +1,313 @@
+#!/usr/bin/env python
+#Copyright 2014 Plexxi, Inc.
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+#
+#	Main entrypoint for the DSE
+#
+#	Configuration in d6cage.ini
+#
+#	Prerequisites:
+#	- Plexxi API libraries (there is an RPM)
+#	- Python dependencies (see readme elsewhere, or capture RPM)
+#
+#
+#
+import amqprouter
+from configobj import ConfigObj
+from d6message import d6msg
+from deepsix import deepSix
+import imp
+import logging
+import pprint
+from Queue import Queue
+import sys
+import threading
+
+
+class d6Cage(deepSix):
+    def __init__(self):
+
+        cfgObj = ConfigObj("d6cage.ini")
+        self.config = cfgObj.dict()
+
+        cageKeys = self.config["d6cage"]['keys']
+        cageDesc = self.config["d6cage"]['description']
+        name = "d6cage"
+
+        deepSix.__init__(self, name, cageKeys)
+
+        self.inbox = Queue()
+        self.dataPath = Queue()
+
+        self.table = amqprouter.routeTable()
+        self.table.add("local.router", self.inbox)
+        self.table.add(self.name, self.inbox)
+        self.table.add("router", self.inbox)
+        localname = "local." + self.name
+        self.table.add(localname, self.inbox)
+
+        self.modules = {}
+        self.services = {}
+
+        self.unloadingServices = {}
+        self.reloadingServices = set()
+
+        self.services[self.name] = {}
+        self.services[self.name]['service'] = self
+        self.services[self.name]['name'] = self.name
+        self.services[self.name]['description'] = cageDesc
+        self.services[self.name]['inbox'] = self.inbox
+        self.services[self.name]['keys'] = self.keys
+
+        self.subscribe(
+            "local.d6cage",
+            "routeKeys",
+            callback=self.updateRoutes,
+            interval=5)
+
+        self.load_modules_from_config()
+        self.load_services_from_config()
+
+    def newConfig(self, msg):
+
+        newConfig = msg.body.data
+
+        if type(newConfig) == dict and newConfig:
+
+            if "modules" in newConfig:
+
+                for module in newConfig["modules"]:
+
+                    if module not in sys.modules:
+
+                        self.loadModule(
+                            module,
+                            newConfig['modules'][module]['filename'])
+
+            if "services" in newConfig:
+
+                for service in newConfig['services']:
+
+                    if service not in self.services:
+
+                        self.createservice(
+                            service,
+                            **newConfig['services'][service])
+
+            self.config = newConfig
+
+    def reloadStoppedService(self, service):
+
+        moduleName = self.config['services'][service]['moduleName']
+
+        try:
+            reload(sys.modules[moduleName])
+        except Exception, errmsg:
+            logging.info(
+                "Unable to reload module '%s': %s", moduleName, errmsg)
+            return
+
+        self.createservice(service, **self.config['services'][service])
+
+    def waitForServiceToStop(
+            self,
+            service,
+            attemptsLeft=20,
+            callback=None,
+            cbkwargs={}):
+
+        if attemptsLeft > 0:
+
+            if self.services[service]['object'].isActive():
+
+                self.timerThreads.append(
+                    threading.Timer(
+                        10,
+                        self.waitForServiceToStop,
+                        [service, attemptsLeft - 1]).start())
+
+            else:
+
+                del self.services[service]
+
+                if callback:
+                    callback(**cbkwargs)
+
+        else:
+            logging.info("Unable to stop service '%s'", service)
+
+    def loadModule(self, name, filename):
+        if name in sys.modules:
+            logging.info(
+                "D6CAGE error loading module '%s': module already exists"
+                % (name))
+            return
+        try:
+            logging.info("********D6CAGE loading module: %s" % (name))
+            imp.load_source(name, filename)
+        except Exception, errmsg:
+            logging.info(
+                "D6CAGE error loading module '%s': %s" % (name, errmsg))
+
+    def load_modules_from_config(self):
+        for section in self.config['modules'].keys():
+            filename = self.config['modules'][section]["filename"]
+
+            self.loadModule(section, filename)
+
+    def createservice(
+            self,
+            name="",
+            keys="",
+            description="",
+            moduleName="",
+            args={}):
+
+        if moduleName not in sys.modules:
+            logging.info(
+                "D6CAGE: error loading service" + name +
+                ": module " + moduleName + " does not exist")
+
+            return
+
+        if name in self.services:
+            logging.info(
+                "D6CAGE: error loading service '%s': name already in use"
+                % (name))
+            return
+
+        inbox = Queue()
+        module = sys.modules[moduleName]
+
+        try:
+            logging.info("********D6CAGE loading service: %s" % (name))
+            svcObject = module.d6service(
+                name,
+                keys,
+                inbox,
+                self.dataPath,
+                args)
+        except Exception, errmsg:
+            logging.exception(
+                "D6CAGE error loading service '%s' of module '%s': %s"
+                % (name, module, errmsg))
+
+        if svcObject:
+
+            self.services[name] = {}
+            self.services[name]['name'] = name
+            self.services[name]['description'] = description
+            self.services[name]['moduleName'] = moduleName
+            self.services[name]['keys'] = keys
+            self.services[name]['args'] = args
+            self.services[name]['object'] = svcObject
+            self.services[name]['inbox'] = inbox
+
+            try:
+                self.services[name]['object'].start()
+                self.table.add(name, inbox)
+                localname = "local." + name
+                self.table.add(localname, inbox)
+                self.subscribe(
+                    name,
+                    'routeKeys',
+                    callback=self.updateRoutes,
+                    interval=5)
+                self.publish('services', self.services)
+            except Exception, errmsg:
+                logging.exception(
+                    "D6CAGE: error starting service '%s': %s" % (name, errmsg))
+                del self.services[name]
+
+    def updateRoutes(self, msg):
+        keyData = self.getSubData(msg.correlationId, sender=msg.replyTo)
+        currentKeys = set(keyData.data)
+        logging.info("D6CAGE updateRoutes msgbody: %s", msg.body.data)
+        pubKeys = set(msg.body.data['keys'])
+
+        if currentKeys != pubKeys:
+
+            newKeys = pubKeys - currentKeys
+
+            if newKeys:
+                self.table.add(
+                    list(newKeys), self.services[msg.replyTo]['inbox'])
+
+            oldKeys = currentKeys - pubKeys
+
+            if oldKeys:
+                self.table.remove(
+                    list(oldKeys), self.services[msg.replyTo]['inbox'])
+
+            return msg.body
+
+    def load_services_from_config(self):
+
+        for section in self.config['services'].keys():
+
+            self.createservice(section, **self.config['services'][section])
+
+    def routemsg(self, msg):
+
+        logging.info(
+            "****** Message lookup %s from %s" % (msg.key, msg.replyTo))
+
+        destinations = self.table.lookup(msg.key)
+
+        if destinations:
+            for destination in destinations:
+                destination.put_nowait(msg)
+                logging.info(
+                    "****** Message sent to %s from %s"
+                    % (msg.key, msg.replyTo))
+
+    def d6reload(self, msg):
+
+        inargs = msg.body.data
+
+        service = inargs['service']
+
+        newmsg = d6msg(key=service, replyTo=self.name, type="shut")
+
+        self.send(newmsg)
+        cbkwargs = {}
+
+        cbkwargs['service'] = service
+
+        self.waitForServiceToStop(
+            service,
+            callback=self.reloadStoppedService,
+            cbkwargs=cbkwargs)
+
+    def cmdhandler(self, msg):
+
+        command = msg.header['dataindex']
+
+        if command == "reload":
+            self.d6reload(msg)
+
+    def d6run(self):
+        if not self.dataPath.empty():
+            msg = self.dataPath.get()
+            self.routemsg(msg)
+            self.dataPath.task_done()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s %(message)s')
+    pp = pprint.PrettyPrinter(indent=1)
+    main = d6Cage()
+    main.start()
+    main.join()

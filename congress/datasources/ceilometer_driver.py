7737d45a917497865b28b810cde24a0f46fb84d9
Madhu Mohan N S <mmohan@mvista.com>
2014-11-03 21:26:33 +0100

    Updated Ceilometer driver as per new datasource driver
    Ceilometer driver now uses Translators commonly refactored in datasource
    driver.
    Also updated the test code to test the ceilometer driver.
    Added call to register_translator().
    
    Fixes bug: 1388967
    
    Change-Id: I2913c1b9cda36548ee38fb90e15a355c1226f38f

--- congress/datasources/ceilometer_driver.py
+++ congress/datasources/ceilometer_driver.py
@@ -14,12 +14,10 @@
 #    under the License.
 #
 import ceilometerclient.client as cc
-import uuid
 
 from congress.datasources.datasource_driver import DataSourceDriver
 from congress.datasources import datasource_utils
 from congress.openstack.common import log as logging
-from congress.utils import value_to_congress
 
 LOG = logging.getLogger(__name__)
 
@@ -44,70 +42,105 @@ class CeilometerDriver(DataSourceDriver):
     EVENT_TRAITS = "events.traits"
     ALARM_THRESHOLD_RULE = "alarms.threshold_rule"
 
+    value_trans = {'translation-type': 'VALUE'}
+
+    meters_translator = {
+        'translation-type': 'HDICT',
+        'table-name': METERS,
+        'selector-type': 'DICT_SELECTOR',
+        'field-translators':
+            ({'fieldname': 'meter_id', 'translator': value_trans},
+             {'fieldname': 'name', 'translator': value_trans},
+             {'fieldname': 'type', 'translator': value_trans},
+             {'fieldname': 'unit', 'translator': value_trans},
+             {'fieldname': 'source', 'translator': value_trans},
+             {'fieldname': 'resource_id', 'translator': value_trans},
+             {'fieldname': 'user_id', 'translator': value_trans},
+             {'fieldname': 'project_id', 'translator': value_trans})}
+
+    alarms_translator = {
+        'translation-type': 'HDICT',
+        'table-name': ALARMS,
+        'selector-type': 'DICT_SELECTOR',
+        'field-translators':
+            ({'fieldname': 'alarm_id', 'translator': value_trans},
+             {'fieldname': 'name', 'translator': value_trans},
+             {'fieldname': 'state', 'translator': value_trans},
+             {'fieldname': 'enabled', 'translator': value_trans},
+             {'fieldname': 'threshold_rule', 'col': 'threshold_rule_id',
+              'translator': {'translation-type': 'VDICT',
+                             'table-name': ALARM_THRESHOLD_RULE,
+                             'id-col': 'threshold_rule_id',
+                             'key-col': 'key', 'val-col': 'value',
+                             'translator': value_trans}},
+             {'fieldname': 'type', 'translator': value_trans},
+             {'fieldname': 'description', 'translator': value_trans},
+             {'fieldname': 'time_constraints', 'translator': value_trans},
+             {'fieldname': 'user_id', 'translator': value_trans},
+             {'fieldname': 'project_id', 'translator': value_trans},
+             {'fieldname': 'alarm_actions', 'translator': value_trans},
+             {'fieldname': 'ok_actions', 'translator': value_trans},
+             {'fieldname': 'insufficient_data_actions', 'translator':
+             value_trans},
+             {'fieldname': 'repeat_actions', 'translator': value_trans},
+             {'fieldname': 'timestamp', 'translator': value_trans},
+             {'fieldname': 'state_timestamp', 'translator': value_trans},
+             )}
+
+    events_translator = {
+        'translation-type': 'HDICT',
+        'table-name': EVENTS,
+        'selector-type': 'DICT_SELECTOR',
+        'field-translators':
+            ({'fieldname': 'message_id', 'translator': value_trans},
+             {'fieldname': 'event_type', 'translator': value_trans},
+             {'fieldname': 'generated', 'translator': value_trans},
+             {'fieldname': 'traits', 'col': 'trait_id',
+              'translator': {'translation-type': 'VDICT',
+              'table-name': EVENT_TRAITS,
+              'id-col': 'trait_id',
+              'key-col': 'key', 'val-col': 'value',
+              'translator': value_trans}})}
+
     def __init__(self, name='', keys='', inbox=None, datapath=None, args=None):
         super(CeilometerDriver, self).__init__(name, keys, inbox,
                                                datapath, args)
         self.creds = self.get_ceilometer_credentials_v2(name, args)
         self.ceilometer_client = cc.get_client(**self.creds)
         self.raw_state = {}
+        self.register_translator(CeilometerDriver.meters_translator)
+        self.register_translator(CeilometerDriver.alarms_translator)
+        self.register_translator(CeilometerDriver.events_translator)
+
+    @classmethod
+    def get_translators(cls):
+        return (cls.meters_translator, cls.alarms_translator,
+        cls.events_translator)
 
     def update_from_datasource(self):
-        self.meters = []
-        self.alarms = []
-        self.alarm_threshold_rule = []
-        self.events = []
-        self.event_traits = []
-
+        """Read Data from Ceilometer datasource to fill
+        up the current state of the policy engine.
+        """
+        LOG.debug("Ceilometer grabbing meters")
         meters = self.ceilometer_client.meters.list()
         if ('meters' not in self.raw_state or meters !=
             self.raw_state['meters']):
             self.raw_state['meters'] = meters
             self._translate_meters(meters)
-        else:
-            self.raw_state['meters'] = meters
-            self._translate_meters(meters)
 
+        LOG.debug("Ceilometer grabbing alarms")
         alarms = self.ceilometer_client.alarms.list()
         if ('alarms' not in self.raw_state or alarms !=
             self.raw_state['alarms']):
             self.raw_state['alarms'] = alarms
             self._translate_alarms(alarms)
-        else:
-            self.alarms = self.state[self.ALARMS]
-            self.alarm_threshold_rule = self.state[self.ALARM_THRESHOLD_RULE]
 
+        LOG.debug("Ceilometer grabbing events")
         events = self.ceilometer_client.events.list()
         if ('events' not in self.raw_state or events !=
             self.raw_state['events']):
             self.raw_state['events'] = events
             self._translate_events(events)
-        else:
-            self.events = self.state[self.EVENTS]
-            self.event_traits = self.state[self.EVENT_TRAITS]
-
-        LOG.debug("EVENTS obtained from ceilometer %s", self.events)
-        # set state
-        # TODO(thinrichs): use self.state everywhere instead of self.meters...
-        self.state = {}
-        self.state[self.METERS] = set(self.meters)
-        self.state[self.ALARMS] = set(self.alarms)
-        self.state[self.ALARM_THRESHOLD_RULE] = set(self.alarm_threshold_rule)
-        self.state[self.EVENTS] = set(self.events)
-        self.state[self.EVENT_TRAITS] = set(self.event_traits)
-
-    @classmethod
-    def get_schema(cls):
-        d = {}
-        d[cls.METERS] = ("meter_id", "name", "type", "unit", "source",
-                         "resource_id", "user_id", "project_id")
-        d[cls.ALARMS] = ("alarm_id", "name", "state", "enabled",
-                         "threshold_rule", "type", "description",
-                         "time_constraints", "user_id", "project_id",
-                         "alarm_actions", "ok_actions",
-                         "insufficient_data_actions", "repeat_actions",
-                         "timestamp", "state_timestamp")
-        d[cls.EVENTS] = ("message_id", "event_type", "generated", "traits")
-        return d
 
     def get_ceilometer_credentials_v2(self, name, args):
         creds = datasource_utils.get_credentials(name, args)
@@ -119,172 +152,65 @@ class CeilometerDriver(DataSourceDriver):
         d['tenant_name'] = creds['tenant_name']
         return d
 
-    @classmethod
-    def meter_key_position_map(cls):
-        d = {}
-        d['meter_id'] = 0
-        d['name'] = 1
-        d['type'] = 2
-        d['unit'] = 3
-        d['source'] = 4
-        d['resource_id'] = 5
-        d['user_id'] = 6
-        d['project_id'] = 7
-        return d
-
-    @classmethod
-    def alarm_key_position_map(cls):
-        d = {}
-        d['alarm_id'] = 0
-        d['name'] = 1
-        d['state'] = 2
-        d['enabled'] = 3
-        d['threshold_rule'] = 4
-        d['type'] = 5
-        d['description'] = 6
-        d['time_constraints'] = 7
-        d['user_id'] = 8
-        d['project_id'] = 9
-        d['alarm_actions'] = 10
-        d['ok_actions'] = 11
-        d['insufficient_data_actions'] = 12
-        d['repeat_actions'] = 13
-        d['timestamp'] = 14
-        d['state_timestamp'] = 15
-        return d
-
-    @classmethod
-    def event_key_position_map(cls):
-        d = {}
-        d['message_id'] = 0
-        d['event_type'] = 1
-        d['generated'] = 2
-        d['traits'] = 3
-        return d
-
     def _translate_meters(self, obj):
-        self.meters = []
-        key_to_index = self.meter_key_position_map()
-        max_meter_index = max(key_to_index.values()) + 1
-        t_list = []
-        for p in obj:
-            if type(p) != type(dict()):
-                p_dict = p.to_dict()
-            else:
-                p_dict = p
-            row = ['None'] * max_meter_index
-            for k, v in p_dict.items():
-                row[key_to_index[k]] = value_to_congress(v)
-            t_list.append(tuple(row))
-        self.meters = t_list
+        """Translate the meters represented by OBJ into tables.
+        Assigns self.state[tablename] for the table names
+        generated from OBJ: METERS
+        """
+        meters = [o.to_dict() for o in obj]
+
+        LOG.debug("METERS: %s" % str(meters))
+
+        row_data = CeilometerDriver.convert_objs(meters,
+                                                 self.meters_translator)
+        self.state[self.METERS] = set()
+        for table, row in row_data:
+            assert table == self.METERS
+            self.state[table].add(row)
+
+        LOG.debug("METERS: %s" % str(self.state[self.METERS]))
 
     def _translate_alarms(self, obj):
-        LOG.debug("Translating ALARM object %s", obj)
-        self.alarms = []
-        self.alarm_threshold_rule = []
-        key_to_index = self.alarm_key_position_map()
-        max_alarm_index = max(key_to_index.values()) + 1
-        t_list = []
-        t_thres_list = []
-        for k in obj:
-            if type(k) != type(dict()):
-                k_dict = k.to_dict()
-            else:
-                k_dict = k
-            row = ['None'] * max_alarm_index
-            for p, v in k_dict.items():
-                if p == 'threshold_rule':
-                    threshold_rule_id = str(uuid.uuid1())
-                    for s, t in v.items():
-                        if type(t) != type(list()) and type(t) != type(dict()):
-                            # FIXME(madhumohan): Dirty workaround. A cleaner
-                            # approach is required to handled None object in
-                            # the data
-                            if t is None:
-                                t = 'None'
-                            row_thres_tuple = (threshold_rule_id, s, t)
-                            t_thres_list.append(row_thres_tuple)
-                    row[key_to_index['threshold_rule']] = threshold_rule_id
-                else:
-                    if p in key_to_index:
-                        row[key_to_index[p]] = value_to_congress(v)
-                    else:
-                        LOG.info("Ignoring unexpected dict key %s", p)
-            t_list.append(tuple(row))
+        """Translate the alarms represented by OBJ into tables.
+        Assigns self.state[tablename] for the table names
+        generated from OBJ: ALARMS, ALARM_THRESHOLD_RULE
+        """
+        alarms = [o.to_dict() for o in obj]
+        LOG.debug("ALARMS: %s" % str(alarms))
 
-        LOG.debug("Generated alarm list %s", t_list)
-        LOG.debug("Generated threshold rule list %s", t_thres_list)
+        row_data = CeilometerDriver.convert_objs(alarms,
+                                                 self.alarms_translator)
+        alarm_tables = (self.ALARMS, self.ALARM_THRESHOLD_RULE)
+        for table in alarm_tables:
+            self.state[table] = set()
+        for table, row in row_data:
+            assert table in alarm_tables
+            self.state[table].add(row)
 
-        self.alarms = t_list
-        self.alarm_threshold_rule = t_thres_list
+        LOG.debug("ALARMS: %s" % str(self.state[self.ALARMS]))
+        LOG.debug("THRESHOLD: %s"
+                  % str(self.state[self.ALARM_THRESHOLD_RULE]))
 
     def _translate_events(self, obj):
-        LOG.debug("Translating EVENT object %s", obj)
-        self.events = []
-        self.event_traits = []
-        key_to_index = self.event_key_position_map()
-        max_event_index = max(key_to_index.values()) + 1
-        t_list = []
-        t_trait_list = []
-        # TODO(madhumohan): Need a modular implementation of the below loop for
-        # better readability and maintainability. Also for flexible translation
-        # all types of nested datastructure in the data.
-        for k in obj:
-            if type(k) != type(dict()):
-                k_dict = k.to_dict()
-            else:
-                k_dict = k
-            row = ['None'] * max_event_index
-            for p, v in k_dict.items():
-                if p == 'traits':
-                    trait_id = str(uuid.uuid1())
-                    for trait in k_dict[p]:
-                        if trait['name'] == 'payload':
-                            t_dict = eval(trait['value'])
-                            for s, t in t_dict.items():
-                                # FIXME(madhumohan): Dictionary items within
-                                # the payload are handled as additional fields
-                                # in the payload table. Need a better way to
-                                # handle dictionaries or other structures
-                                # within payload Nested dictionaries in the
-                                # payload are skipped Lists within the
-                                # dictionaries are also ignored
-                                if type(t) == type(dict()):
-                                    for n, m in t.items():
-                                        if type(m) != type(dict()) and \
-                                           type(m) != type(list()):
-                                            # FIXME(madhumohan): Dirty
-                                            # workaround. A cleaner approach
-                                            # is required to handled None
-                                            # object in the data
-                                            if m is None:
-                                                m = 'None'
-                                            row_trait_tuple = \
-                                                (trait_id, n, m)
-                                            t_trait_list.append(
-                                                row_trait_tuple)
-                                else:
-                                    if type(t) != type(list()):
-                                        # FIXME(madhumohan): Dirty workaround.
-                                        # A cleaner approach is required to
-                                        # handled None object in the data
-                                        if t is None:
-                                            t = 'None'
-                                        row_trait_tuple = (trait_id, s, t)
-                                        t_trait_list.append(row_trait_tuple)
-                    row[key_to_index['traits']] = trait_id
-                else:
-                    if p in key_to_index:
-                        row[key_to_index[p]] = value_to_congress(v)
-                    else:
-                        LOG.info("Ignoring unexpected dict key %s", p)
-            t_list.append(tuple(row))
+        """Translate the events represented by OBJ into tables.
+        Assigns self.state[tablename] for the table names
+        generated from OBJ: EVENTS, EVENT_TRAITS.
+        """
+        events = [o.to_dict() for o in obj]
+        LOG.debug("EVENTS: %s" % str(events))
 
-        LOG.debug("Generated event list %s", t_list)
-        LOG.debug("Generated trait list %s", t_trait_list)
+        row_data = CeilometerDriver.convert_objs(events,
+                                                 self.events_translator)
 
-        self.events = t_list
-        self.event_traits = t_trait_list
+        event_tables = (self.EVENTS, self.EVENT_TRAITS)
+        for table in event_tables:
+            self.state[table] = set()
+        for table, row in row_data:
+            assert table in event_tables
+            self.state[table].add(row)
+
+        LOG.debug("EVENTS: %s" % str(self.state[self.EVENTS]))
+        LOG.debug("TRAITS: %s" % str(self.state[self.EVENT_TRAITS]))
 
 
 def main():
--- congress/tests/datasources/test_ceilometer_driver.py
+++ congress/tests/datasources/test_ceilometer_driver.py
@@ -33,14 +33,6 @@ class TestCeilometerDriver(base.TestCase):
 
     def test_list_meters(self):
         meters_data = [
-            ResponseObj({'name': 'network.incoming.bytes',
-                         'type': 'cumulative',
-                         'unit': 'B',
-                         'resource_id': 'instance-00000001-tap437ce69c-e5',
-                         'user_id': '2b01323fd71345bc8cdc5dbbd6d127ea',
-                         'project_id': '0020df0171ec41b597cd8b3002e21bee',
-                         'meter_id': 'aW5zdGFuY2UtMDAwMDAwMDEtYTI1N2JhMT',
-                         'source': 'openstack'}),
             ResponseObj({'name': 'instance:m1.tiny',
                          'type': 'gauge',
                          'unit': 'instance',
@@ -48,12 +40,26 @@ class TestCeilometerDriver(base.TestCase):
                          'user_id': '2b01323fd71345bc8cdc5dbbd6d127ea',
                          'project_id': '0020df0171ec41b597cd8b3002e21bee',
                          'meter_id': 'YTI1N2JhMTMtMGIzNi00YTg2LWFlODktZjc4ZG',
+                         'source': 'openstack'}),
+            ResponseObj({'name': 'network.incoming.bytes',
+                         'type': 'cumulative',
+                         'unit': 'B',
+                         'resource_id': 'instance-00000001-tap437ce69c-e5',
+                         'user_id': '2b01323fd71345bc8cdc5dbbd6d127ea',
+                         'project_id': '0020df0171ec41b597cd8b3002e21bee',
+                         'meter_id': 'aW5zdGFuY2UtMDAwMDAwMDEtYTI1N2JhMT',
                          'source': 'openstack'})]
 
         self.driver._translate_meters(meters_data)
-        meter_list = self.driver.meters
+        meter_list = list(self.driver.state['meters'])
         self.assertIsNotNone(meter_list)
         self.assertEqual(2, len(meter_list))
+
+        for meter in meter_list:
+            if meter[1] == 'network.incoming.bytes':
+                meter1 = meter
+            elif meter[1] == 'instance:m1.tiny':
+                meter2 = meter
 
         # Verifying individual tuple data
         self.assertEqual(('aW5zdGFuY2UtMDAwMDAwMDEtYTI1N2JhMT',
@@ -64,7 +70,7 @@ class TestCeilometerDriver(base.TestCase):
                           'instance-00000001-tap437ce69c-e5',
                           '2b01323fd71345bc8cdc5dbbd6d127ea',
                           '0020df0171ec41b597cd8b3002e21bee'),
-                         meter_list[0])
+                         meter1)
 
         self.assertEqual(('YTI1N2JhMTMtMGIzNi00YTg2LWFlODktZjc4ZG',
                           'instance:m1.tiny',
@@ -74,30 +80,17 @@ class TestCeilometerDriver(base.TestCase):
                           'a257ba13-0b36-4a86-ae89-f78dd28b8ae5',
                           '2b01323fd71345bc8cdc5dbbd6d127ea',
                           '0020df0171ec41b597cd8b3002e21bee'),
-                         meter_list[1])
+                         meter2)
 
     def test_list_alarms(self):
         threshold_rule1 = {'key1': 'value1',
                            'key2': 'value2',
                            'key3': 'value3'}
+        threshold_rule2 = {'key4': 'value4',
+                           'key5': 'value5',
+                           'key6': 'value6'}
 
         alarms_data = [
-            ResponseObj({'alarm_id': 'd1b2b7a7-9512-4290-97ca-2580ed72c375',
-                         'name': 'cpu_high',
-                         'state': 'insufficient data',
-                         'enabled': 'True',
-                         'threshold_rule': threshold_rule1,
-                         'type': 'threshold',
-                         'description': 'instance running hot',
-                         'time_constraints': '[]',
-                         'user_id': '2b01323fd71345bc8cdc5dbbd6d127ea',
-                         'project_id': '',
-                         'alarm_actions': "[u'log://']",
-                         'ok_actions': '[]',
-                         'insufficient_data_actions': '[]',
-                         'repeat_actions': 'False',
-                         'timestamp': '2014-09-30T04:55:36.015925',
-                         'state_timestamp': '2014-09-30T04:55:36.015925'}),
             ResponseObj({'alarm_id': '7ef99553-a73f-4b18-a617-997a479c48e9',
                          'name': 'cpu_high2',
                          'state': 'insufficient data',
@@ -113,76 +106,110 @@ class TestCeilometerDriver(base.TestCase):
                          'insufficient_data_actions': '[]',
                          'repeat_actions': 'False',
                          'timestamp': '2014-09-30T05:00:43.351041',
-                         'state_timestamp': '2014-09-30T05:00:43.351041'})]
+                         'state_timestamp': '2014-09-30T05:00:43.351041'}),
+            ResponseObj({'alarm_id': 'd1b2b7a7-9512-4290-97ca-2580ed72c375',
+                         'name': 'cpu_high',
+                         'state': 'insufficient data',
+                         'enabled': 'True',
+                         'threshold_rule': threshold_rule2,
+                         'type': 'threshold',
+                         'description': 'instance running hot',
+                         'time_constraints': '[]',
+                         'user_id': '2b01323fd71345bc8cdc5dbbd6d127ea',
+                         'project_id': '',
+                         'alarm_actions': "[u'log://']",
+                         'ok_actions': '[]',
+                         'insufficient_data_actions': '[]',
+                         'repeat_actions': 'False',
+                         'timestamp': '2014-09-30T04:55:36.015925',
+                         'state_timestamp': '2014-09-30T04:55:36.015925'})]
 
         self.driver._translate_alarms(alarms_data)
-        alarm_list = self.driver.alarms
+        alarm_list = list(self.driver.state['alarms'])
         self.assertIsNotNone(alarm_list)
         self.assertEqual(2, len(alarm_list))
 
-        alarm_threshold_rule = self.driver.alarm_threshold_rule
+        alarm_threshold_rule = list(self.driver.state['alarms.threshold_rule'])
         self.assertIsNotNone(alarm_threshold_rule)
         self.assertEqual(6, len(alarm_threshold_rule))
 
-        thresh_rule_id1 = alarm_threshold_rule[0][0]
-        thresh_rule_id2 = alarm_threshold_rule[3][0]
+        for alarm in alarm_list:
+            if alarm[1] == 'cpu_high2':
+                alarm1 = alarm
+            elif alarm[1] == 'cpu_high':
+                alarm2 = alarm
+
+        for thres in alarm_threshold_rule:
+            if thres[1] in ['key1', 'key2', 'key3']:
+                thresh_rule_id1 = thres[0]
+            elif thres[1] in ['key4', 'key5', 'key6']:
+                thresh_rule_id2 = thres[0]
 
         # Verifying individual tuple data
-        self.assertEqual(('d1b2b7a7-9512-4290-97ca-2580ed72c375',
-                          'cpu_high', 'insufficient data', 'True',
-                          thresh_rule_id1,
-                          'threshold', 'instance running hot',
-                          '[]', '2b01323fd71345bc8cdc5dbbd6d127ea',
-                          '', "[u'log://']", '[]', '[]',
-                          'False', '2014-09-30T04:55:36.015925',
-                          '2014-09-30T04:55:36.015925'),
-                         alarm_list[0])
-
         self.assertEqual(('7ef99553-a73f-4b18-a617-997a479c48e9',
                           'cpu_high2', 'insufficient data',
                           'True',
-                          thresh_rule_id2,
+                          thresh_rule_id1,
                           'threshold', 'instance running hot',
                           '[]', '2b01323fd71345bc8cdc5dbbd6d127ea',
                           '', "[u'log://']", '[]', '[]', 'False',
                           '2014-09-30T05:00:43.351041',
                           '2014-09-30T05:00:43.351041'),
-                         alarm_list[1])
+                         alarm1)
+        self.assertEqual(('d1b2b7a7-9512-4290-97ca-2580ed72c375',
+                          'cpu_high', 'insufficient data', 'True',
+                          thresh_rule_id2,
+                          'threshold', 'instance running hot',
+                          '[]', '2b01323fd71345bc8cdc5dbbd6d127ea',
+                          '', "[u'log://']", '[]', '[]',
+                          'False', '2014-09-30T04:55:36.015925',
+                          '2014-09-30T04:55:36.015925'),
+                         alarm2)
 
     def test_list_events(self):
-        trait = [{'name': 'payload',
-                  'value': "{'t1': 'value1', 't2': 'value2', 't3': 'value3'}"}]
+        trait1 = {'t1': 'value1', 't2': 'value2', 't3': 'value3'}
+        trait2 = {'t4': 'value4', 't5': 'value5', 't6': 'value6'}
+
         events_data = [
             ResponseObj({'message_id': '6834861c-ccb3-4c6f-ac00-fe8fe1ad4ed4',
                          'event_type': 'image.create',
                          'generated': '2014-09-29T08:19:45.556301',
-                         'traits': trait}),
+                         'traits': trait1}),
             ResponseObj({'message_id': '3676d6d4-5c65-4442-9eda-b78d750ea91f',
                          'event_type': 'compute.instance.update',
                          'generated': '2014-09-30T04:54:45.395522',
-                         'traits': trait})]
+                         'traits': trait2})]
 
         self.driver._translate_events(events_data)
-        event_list = self.driver.events
+        event_list = list(self.driver.state['events'])
         self.assertIsNotNone(event_list)
         self.assertEqual(2, len(event_list))
 
-        event_trait_list = self.driver.event_traits
+        event_trait_list = list(self.driver.state['events.traits'])
         self.assertIsNotNone(event_trait_list)
         self.assertEqual(6, len(event_trait_list))
 
-        event_trait1 = event_trait_list[0][0]
-        event_trait2 = event_trait_list[3][0]
+        for event in event_list:
+            if event[1] == 'image.create':
+                event1 = event
+            elif event[1] == 'compute.instance.update':
+                event2 = event
+
+        for trait in event_trait_list:
+            if trait[1] in ['t1', 't2', 't3']:
+                event_trait1 = trait[0]
+            elif trait[1] in ['t4', 't5', 't6']:
+                event_trait2 = trait[0]
 
         # check an individual user entry
         self.assertEqual(('6834861c-ccb3-4c6f-ac00-fe8fe1ad4ed4',
                           'image.create',
                           '2014-09-29T08:19:45.556301',
                           event_trait1),
-                         event_list[0])
+                         event1)
 
         self.assertEqual(('3676d6d4-5c65-4442-9eda-b78d750ea91f',
                           'compute.instance.update',
                           '2014-09-30T04:54:45.395522',
                           event_trait2),
-                         event_list[1])
+                         event2)

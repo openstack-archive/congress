2e693cc897f91dd2c919cf76fbd0c4321790d1f2
Aaron Rosen <aaronorosen@gmail.com>
2014-12-16 13:13:22 -0800

    Python 3: Fix import path for d6message in deepsix
    This patch fixes the import path to be the full path for d6message.
    This was causing an ImportError for python3. In addition, we should
    have been using the full path anyways. Lastly, this import should not
    be grouped with the stdlibs.
    
    Closes-bug: 1403209
    Partially implements blueprint: support-python3
    
    Change-Id: I0d721446401225523b7acbab0a3514b53056f815

--- congress/dse/deepsix.py
+++ congress/dse/deepsix.py
@@ -12,12 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from d6message import d6msg
 import eventlet
 from eventlet import greenthread
 from eventlet import hubs
 eventlet.monkey_patch()
 
+from congress.dse.d6message import d6msg
 from congress.dse.dataobj import dataObject
 from congress.dse.dataobj import pubData
 from congress.dse.dataobj import subData

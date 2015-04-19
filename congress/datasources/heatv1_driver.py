f11fe36bce1773170f5d674d09490170c7dd6e81
Prajakta Naik <nkprajakta@gmail.com>
2015-04-18 22:31:50 -0700

    Heat datasource driver
    Implements-blueprint: heat-datasource-driver
    Change-Id: Icb0d13e70d42b6c7b014cb056f38b7ef999acb62

---
+++ congress/datasources/heatv1_driver.py
@@ -0,0 +1,153 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import heatclient.v1.client as heatclient
+import keystoneclient.v2_0.client as ksclient
+
+from congress.datasources import datasource_driver
+from congress.datasources import datasource_utils
+from congress.openstack.common import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+def d6service(name, keys, inbox, datapath, args):
+    """This method is called by d6cage to create a dataservice instance."""
+    return HeatV1Driver(name, keys, inbox, datapath, args)
+
+
+class HeatV1Driver(datasource_driver.DataSourceDriver):
+
+    STACKS = "stacks"
+    RESOURCES = "resources"
+    EVENTS = "events"
+    LINKS = "links"
+    CONFIGS = "configs"
+    DEPLOYMENTS = "deployments"
+
+    # TODO(thinrichs): add resources, events, snapshots
+    value_trans = {'translation-type': 'VALUE'}
+    stacks_links_translator = {
+        'translation-type': 'HDICT',
+        'table-name': 'stacks_links',
+        'parent-key': 'id',
+        'selector-type': 'DICT_SELECTOR',
+        'in-list': True,
+        'field-translators':
+            ({'fieldname': 'href', 'translator': value_trans},
+             {'fieldname': 'rel', 'translator': value_trans})}
+
+    stacks_translator = {
+        'translation-type': 'HDICT',
+        'table-name': STACKS,
+        'selector-type': 'DICT_SELECTOR',
+        'field-translators':
+        ({'fieldname': 'id', 'translator': value_trans},
+         {'fieldname': 'stack_name', 'translator': value_trans},
+         {'fieldname': 'description', 'translator': value_trans},
+         {'fieldname': 'creation_time', 'translator': value_trans},
+         {'fieldname': 'updated_time', 'translator': value_trans},
+         {'fieldname': 'stack_status', 'translator': value_trans},
+         {'fieldname': 'stack_status_reason', 'translator': value_trans},
+         {'fieldname': 'stack_owner', 'translator': value_trans},
+         {'fieldname': 'parent', 'translator': value_trans},
+         {'fieldname': 'links', 'translator': stacks_links_translator})}
+
+    deployments_output_values_translator = {
+        'translation-type': 'HDICT',
+        'table-name': 'deployment_output_values',
+        'parent-key': 'id',
+        'selector-type': 'DICT_SELECTOR',
+        'field-translators':
+            ({'fieldname': 'deploy_stdout', 'translator': value_trans},
+             {'fieldname': 'deploy_stderr', 'translator': value_trans},
+             {'fieldname': 'deploy_status_code', 'translator': value_trans},
+             {'fieldname': 'result', 'translator': value_trans})}
+
+    software_deployment_translator = {
+        'translation-type': 'HDICT',
+        'table-name': DEPLOYMENTS,
+        'selector-type': 'DICT_SELECTOR',
+        'field-translators':
+        ({'fieldname': 'status', 'translator': value_trans},
+         {'fieldname': 'server_id', 'translator': value_trans},
+         {'fieldname': 'config_id', 'translator': value_trans},
+         {'fieldname': 'action', 'translator': value_trans},
+         {'fieldname': 'status_reason', 'translator': value_trans},
+         {'fieldname': 'id', 'translator': value_trans},
+         {'fieldname': 'output_values',
+          'translator': deployments_output_values_translator})}
+
+    TRANSLATORS = [stacks_translator, software_deployment_translator]
+
+    def __init__(self, name='', keys='', inbox=None, datapath=None, args=None):
+        super(HeatV1Driver, self).__init__(name, keys, inbox, datapath, args)
+        self.creds = args
+
+        keystone = ksclient.Client(**self.creds)
+        endpoint = keystone.service_catalog.url_for(
+            service_type='orchestration', endpoint_type='publicURL')
+        self.heat = heatclient.Client(endpoint, token=keystone.auth_token)
+        self.initialized = True
+
+    @staticmethod
+    def get_datasource_info():
+        result = {}
+        result['id'] = 'heat'
+        result['description'] = ('Datasource driver that interfaces with'
+                                 'Openstack Openstack orchestration aka heat.')
+        result['config'] = datasource_utils.get_openstack_required_config()
+        result['secret'] = ['password']
+        return result
+
+    def update_from_datasource(self):
+        """Called when it is time to pull new data from this datasource.
+
+        Sets self.state[tablename] = <set of tuples of strings/numbers>
+        for every tablename exported by this datasource.
+        """
+        LOG.debug("Grabbing Heat Stacks")
+        self.state = {}
+        self._translate_stacks({'stacks': self.heat.stacks.list()})
+
+        LOG.debug("Grabbing the software deployments")
+        deploy_list = self.heat.software_deployments
+        self._translate_software_deployment({'deployments':
+                                            deploy_list.list()})
+
+    def _translate_stacks(self, obj):
+        """Translate the stacks represented by OBJ into tables.
+
+        Assigns self.state[tablename] for all those TABLENAMEs
+        generated from OBJ: STACKS
+        """
+        LOG.debug("STACKS: %s", str(dict(obj)))
+        row_data = HeatV1Driver.convert_objs(
+            obj['stacks'], HeatV1Driver.stacks_translator)
+        self.state[self.STACKS] = set()
+        self.state['stacks_links'] = set()
+        self.state['deployment_output_values'] = set()
+        for table, row in row_data:
+            self.state[table].add(row)
+
+    def _translate_software_deployment(self, obj):
+        """Translate the stacks represented by OBJ into tables.
+
+        Assigns self.state[tablename] for all those TABLENAMEs
+        generated from OBJ: STACKS
+        """
+        LOG.debug("Software Deployments: %s", str(dict(obj)))
+        row_data = HeatV1Driver.convert_objs(
+            obj['deployments'], HeatV1Driver.software_deployment_translator)
+        self.state[self.DEPLOYMENTS] = set()
+        for table, row in row_data:
+            self.state[table].add(row)
---
+++ congress/tests/datasources/test_heatv1_driver.py
@@ -0,0 +1,106 @@
+# Copyright (c) 2013 VMware, Inc. All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+import contextlib
+
+import mock
+
+from congress.datasources import heatv1_driver
+from congress.tests import base
+from congress.tests import helper
+
+
+class TestHeatV1Driver(base.TestCase):
+
+    def setUp(self):
+        super(TestHeatV1Driver, self).setUp()
+        self.keystone_client_p = mock.patch(
+            "keystoneclient.v2_0.client.Client")
+        self.keystone_client_p.start()
+        self.heat_client_p = mock.patch("heatclient.v1.client.Client")
+        self.heat_client_p.start()
+
+        args = helper.datasource_openstack_args()
+        args['poll_time'] = 0
+        args['client'] = mock.MagicMock()
+        self.driver = heatv1_driver.HeatV1Driver(args=args)
+
+        self.mock_stacks = {'stacks': [
+            {u'id': u'da4e63e2-f79b-4cbb-bee8-33b2a9bd1ac8',
+             u'stack_name': u'my-stack',
+             u'description':
+             u'Simple template to deploy a single compute instance ',
+             u'creation_time': u'2015-04-25T21:20:35Z',
+             u'updated_time': u'None',
+             u'stack_status': u'CREATE_COMPLETE',
+             u'stack_status_reason': u'Stack CREATE completed successfully',
+             u'stack_owner': u'demo',
+             u'parent': u'None',
+             u'links': [
+                    {u'href': u'http://192.168.123.200:8004/v1',
+                     u'rel': u'self'}]}]}
+
+        self.mock_software_deployments = {'deployments': [
+            {u'status': u'COMPLETE',
+             u'server_id': u'ec14c864-096e-4e27-bb8a-2c2b4dc6f3f5',
+             u'config_id': u'8da95794-2ad9-4979-8ae5-739ce314c5cd',
+             u'action': u'CREATE',
+             u'status_reason': u'Outputs received',
+             u'id': u'ef422fa5-719a-419e-a10c-72e3a367b0b8',
+             u'output_values': {
+                 u'deploy_stdout': u'Writing to /tmp/barmy\n',
+                 u'deploy_stderr': u'+ echo Writing to /tmp/barmy\n',
+                 u'deploy_status_code': u'0',
+                 u'result': u'The file /tmp/barmy contains fu for server'}}]}
+
+    def test_update_from_datasource(self):
+        dep = self.mock_software_deployments
+        with contextlib.nested(
+                mock.patch.object(self.driver.heat.stacks,
+                                  "list",
+                                  return_value=self.mock_stacks['stacks']),
+                mock.patch.object(self.driver.heat.software_deployments,
+                                  "list",
+                                  return_value=dep['deployments']),
+                ) as (list, list):
+                self.driver.update_from_datasource()
+        expected = {
+            'stacks': set([
+                (u'da4e63e2-f79b-4cbb-bee8-33b2a9bd1ac8',
+                 u'my-stack',
+                 u'Simple template to deploy a single compute instance ',
+                 u'2015-04-25T21:20:35Z',
+                 u'None',
+                 u'CREATE_COMPLETE',
+                 u'Stack CREATE completed successfully',
+                 u'demo',
+                 u'None')]),
+            'stacks_links': set([
+                (u'da4e63e2-f79b-4cbb-bee8-33b2a9bd1ac8',
+                 u'http://192.168.123.200:8004/v1',
+                 u'self')]),
+            'deployments': set([
+                (u'COMPLETE',
+                 u'ec14c864-096e-4e27-bb8a-2c2b4dc6f3f5',
+                 u'8da95794-2ad9-4979-8ae5-739ce314c5cd',
+                 u'CREATE',
+                 u'Outputs received',
+                 u'ef422fa5-719a-419e-a10c-72e3a367b0b8')]),
+            'deployment_output_values': set([
+                (u'ef422fa5-719a-419e-a10c-72e3a367b0b8',
+                 u'Writing to /tmp/barmy\n',
+                 u'+ echo Writing to /tmp/barmy\n',
+                 u'0',
+                 u'The file /tmp/barmy contains fu for server')])}
+        self.assertEqual(self.driver.state, expected)

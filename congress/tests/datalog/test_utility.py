7943043010513b332d6a5876daafc7db298db5ef
Torin Sandall <torinsandall@gmail.com>
2015-05-24 22:10:40 -0700

    Fix iterstr to return string representations
    Previously iterstr was not implementing __str__ or __repr__ so the string
    representation was not useful for debugging purposes. The new implementation
    lazily invokes str() or repr() on the elements accordingly.
    
    Closes-Bug: 1415205
    Change-Id: I22ed9d9449d414ae086c53433853e65233d2df78

--- congress/tests/datalog/test_utility.py
+++ congress/tests/datalog/test_utility.py
@@ -12,6 +12,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 #
+import contextlib
+import logging as python_logging
+import StringIO
 
 from congress.datalog import utility
 from congress.openstack.common import log as logging
@@ -339,3 +342,64 @@ class TestBagGraph(base.TestCase):
         self.assertTrue(g1.has_cycle())
         g1.delete_edge(2, 4)
         self.assertFalse(g1.has_cycle())
+
+
+class TestIterstr(base.TestCase):
+    class X(object):
+        def __init__(self, v):
+            self.v = v
+
+        def __str__(self):
+            return "%s" % self.v
+
+        def __repr__(self):
+            return "X:%s" % self.v
+
+    @contextlib.contextmanager
+    def get_logging_fixtures(self):
+        stream = StringIO.StringIO()
+        handler = python_logging.StreamHandler(stream)
+        try:
+            logger = python_logging.getLogger(self.__class__.__name__)
+            logger.setLevel(python_logging.INFO)
+            handler.setLevel(python_logging.INFO)
+            logger.addHandler(handler)
+            try:
+                yield (stream, handler, logger)
+            finally:
+                logger.removeHandler(handler)
+        finally:
+            handler.close()
+
+    def test__str__returns_informal_representation(self):
+        xs = map(TestIterstr.X, range(5))
+        observed = utility.iterstr(xs)
+        self.assertEqual("[0;1;2;3;4]", str(observed))
+        self.assertEqual("[0;1;2;3;4]", "{}".format(observed))
+        self.assertEqual("[0;1;2;3;4]", "%s" % observed)
+
+    def test__repr__returns_formal_representation(self):
+        xs = map(TestIterstr.X, range(5))
+        observed = utility.iterstr(xs)
+        self.assertEqual("[X:0;X:1;X:2;X:3;X:4]", repr(observed))
+        self.assertEqual("[X:0;X:1;X:2;X:3;X:4]", "{!r}".format(observed))
+        self.assertEqual("[X:0;X:1;X:2;X:3;X:4]", "%r" % observed)
+
+    def test_empty(self):
+        xs = map(TestIterstr.X, range(0))
+        observed = utility.iterstr(xs)
+        self.assertEqual("[]", str(observed))
+        self.assertEqual("[]", repr(observed))
+
+    def test_logging_basic_integration(self):
+        with self.get_logging_fixtures() as (stream, handler, logger):
+            iterable = utility.iterstr(map(TestIterstr.X, range(5)))
+            logger.info("some message %s", iterable)
+            handler.flush()
+            self.assertEqual("some message [0;1;2;3;4]\n", stream.getvalue())
+
+    def test_logging_skips_interpolation(self):
+        with self.get_logging_fixtures() as (stream, handler, logger):
+            iterable = utility.iterstr(map(TestIterstr.X, range(5)))
+            logger.debug("some message %s", iterable)
+            self.assertIsNone(iterable._str_interp)

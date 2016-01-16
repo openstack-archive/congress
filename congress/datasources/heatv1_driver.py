8751eab43717bde8b9339d9c2a74d127f85323a1
David Höppner <0xffea@gmail.com>
2016-01-16 20:13:25 +0100

    Add resources and events sources to heat driver
    New data sources for resources and events from Heat.
    
    Change-Id: Ib1bba7d9fc2c796d66f5bc6974206a012d9802fb
    Closes-bug: #1534972
    Signed-off-by: David Höppner <0xffea@gmail.com>

--- congress/datasources/heatv1_driver.py
+++ congress/datasources/heatv1_driver.py
@@ -36,8 +36,12 @@ class HeatV1Driver(datasource_driver.PollingDataSourceDriver,
     STACKS_LINKS = "stacks_links"
     DEPLOYMENTS = "deployments"
     DEPLOYMENT_OUTPUT_VALUES = "deployment_output_values"
+    RESOURCES = "resources"
+    RESOURCES_LINKS = "resources_links"
+    EVENTS = "events"
+    EVENTS_LINKS = "events_links"
 
-    # TODO(thinrichs): add resources, events, snapshots
+    # TODO(thinrichs): add snapshots
     value_trans = {'translation-type': 'VALUE'}
     stacks_links_translator = {
         'translation-type': 'HDICT',
@@ -90,7 +94,59 @@ class HeatV1Driver(datasource_driver.PollingDataSourceDriver,
          {'fieldname': 'output_values',
           'translator': deployments_output_values_translator})}
 
-    TRANSLATORS = [stacks_translator, software_deployment_translator]
+    resources_links_translator = {
+        'translation-type': 'HDICT',
+        'table-name': RESOURCES_LINKS,
+        'parent-key': 'physical_resource_id',
+        'selector-type': 'DICT_SELECTOR',
+        'in-list': True,
+        'field-translators':
+            ({'fieldname': 'href', 'translator': value_trans},
+             {'fieldname': 'rel', 'translator': value_trans})}
+
+    resources_translator = {
+        'translation-type': 'HDICT',
+        'table-name': RESOURCES,
+        'selector-type': 'DICT_SELECTOR',
+        'field-translators':
+        ({'fieldname': 'physical_resource_id', 'translator': value_trans},
+         {'fieldname': 'logical_resource_id', 'translator': value_trans},
+         {'fieldname': 'stack_id', 'translator': value_trans},
+         {'fieldname': 'resource_name', 'translator': value_trans},
+         {'fieldname': 'resource_type', 'translator': value_trans},
+         {'fieldname': 'creation_time', 'translator': value_trans},
+         {'fieldname': 'updated_time', 'translator': value_trans},
+         {'fieldname': 'resource_status', 'translator': value_trans},
+         {'fieldname': 'resource_status_reason', 'translator': value_trans},
+         {'fieldname': 'links', 'translator': resources_links_translator})}
+
+    events_links_translator = {
+        'translation-type': 'HDICT',
+        'table-name': EVENTS_LINKS,
+        'parent-key': 'id',
+        'selector-type': 'DICT_SELECTOR',
+        'in-list': True,
+        'field-translators':
+            ({'fieldname': 'href', 'translator': value_trans},
+             {'fieldname': 'rel', 'translator': value_trans})}
+
+    events_translator = {
+        'translation-type': 'HDICT',
+        'table-name': EVENTS,
+        'selector-type': 'DICT_SELECTOR',
+        'field-translators':
+        ({'fieldname': 'id', 'translator': value_trans},
+         {'fieldname': 'physical_resource_id', 'translator': value_trans},
+         {'fieldname': 'logical_resource_id', 'translator': value_trans},
+         {'fieldname': 'stack_id', 'translator': value_trans},
+         {'fieldname': 'resource_name', 'translator': value_trans},
+         {'fieldname': 'event_time', 'translator': value_trans},
+         {'fieldname': 'resource_status', 'translator': value_trans},
+         {'fieldname': 'resource_status_reason', 'translator': value_trans},
+         {'fieldname': 'links', 'translator': events_links_translator})}
+
+    TRANSLATORS = [stacks_translator, software_deployment_translator,
+                   resources_translator, events_translator]
 
     def __init__(self, name='', keys='', inbox=None, datapath=None, args=None):
         super(HeatV1Driver, self).__init__(name, keys, inbox, datapath, args)
@@ -116,12 +172,42 @@ class HeatV1Driver(datasource_driver.PollingDataSourceDriver,
     def update_from_datasource(self):
         """Called when it is time to pull new data from this datasource."""
         LOG.debug("Grabbing Heat Stacks")
-        stacks = {'stacks': self.heat.stacks.list()}
-        self._translate_stacks(stacks)
+        stacks = self.heat.stacks.list()
+        self._translate_stacks({'stacks': stacks})
+
+        LOG.debug("Grabbing resources")
+        stacks = self.heat.stacks.list()
+        resources = self._get_resources(stacks)
+        self._translate_resources(resources)
+
+        LOG.debug("Grabbing events")
+        stacks = self.heat.stacks.list()
+        events = self._get_events(stacks)
+        self._translate_events(events)
 
         LOG.debug("Grabbing the software deployments")
         deployments = {'deployments': self.heat.software_deployments.list()}
         self._translate_software_deployment(deployments)
+
+    def _get_resources(self, stacks):
+        rval = []
+        for stack in stacks:
+            resources = self.heat.resources.list(stack.id)
+            for resource in resources:
+                resource = resource.to_dict()
+                resource['stack_id'] = stack.id
+                rval.append(resource)
+        return {'resources': rval}
+
+    def _get_events(self, stacks):
+        rval = []
+        for stack in stacks:
+            events = self.heat.events.list(stack.id)
+            for event in events:
+                event = event.to_dict()
+                event['stack_id'] = stack.id
+                rval.append(event)
+        return {'events': rval}
 
     @ds_utils.update_state_on_changed(STACKS)
     def _translate_stacks(self, obj):
@@ -137,6 +223,22 @@ class HeatV1Driver(datasource_driver.PollingDataSourceDriver,
         LOG.debug("Software Deployments: %s", str(dict(obj)))
         row_data = HeatV1Driver.convert_objs(
             obj['deployments'], HeatV1Driver.software_deployment_translator)
+        return row_data
+
+    @ds_utils.update_state_on_changed(RESOURCES)
+    def _translate_resources(self, obj):
+        """Translate the resources represented by OBJ into tables."""
+        LOG.debug("Resources: %s", str(dict(obj)))
+        row_data = HeatV1Driver.convert_objs(
+            obj['resources'], HeatV1Driver.resources_translator)
+        return row_data
+
+    @ds_utils.update_state_on_changed(EVENTS)
+    def _translate_events(self, obj):
+        """Translate the events represented by OBJ into tables."""
+        LOG.debug("Events: %s", str(dict(obj)))
+        row_data = HeatV1Driver.convert_objs(
+            obj['events'], HeatV1Driver.events_translator)
         return row_data
 
     def execute(self, action, action_args):

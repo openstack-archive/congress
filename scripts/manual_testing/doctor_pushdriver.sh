1737e41920f8860ca93b8b304958b6ff759541ef
Eric Kao <ekcs.openstack@gmail.com>
2017-04-17 16:22:02 -0700

    Manual testing scripts
    Add to repo so developers can use the scripts
    in manual testing as well as contribute additional scripts.
    
    One step toward systematic pre-release QA.
    Closes-Bug: 1651926
    
    Backport: testing-only change
    
    Change-Id: I1dc7cea2fd3b56d838ae19bd6ef854fcc4b48c8e
    (cherry picked from commit aafdc9ba42bfdf516a09db29b2ebb3c3e9120222)

---
+++ scripts/manual_testing/doctor_pushdriver.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+#############################################################################
+### doctor push data driver manual testing script                         ###
+#
+#   Note:
+#   The following OpenStack environment variables must set first:
+#   OS_USERNAME, OS_PASSWORD, OS_PROJECT_NAME, OS_TENANT_NAME, OS_AUTH_URL
+#   For example by running (under a devstack setup)
+#     $ source devstack/openrc admin admin
+#############################################################################
+
+UUID=`openstack congress datasource create doctor doctor -f value | sed -n '5p'`
+
+openstack congress datasource row update doctor events '[{"id": "0123-4567-89ab", "time": "2016-02-22T11:48:55Z", "type": "compute.host.down", "details": {"hostname": "compute1", "status": "down", "monitor": "zabbix1", "monitor_event_id": "111"}}]'
+
+openstack congress datasource row list doctor events
+
+openstack congress datasource row update $UUID events '[{"id": "1123-4567-89ab", "time": "2016-02-22T11:48:55Z", "type": "compute.host.down", "details": {"hostname": "compute2", "status": "down", "monitor": "zabbix2", "monitor_event_id": "222"}}]'
+
+openstack congress datasource row list doctor events
+
+openstack congress datasource delete doctor

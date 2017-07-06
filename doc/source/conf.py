3c872602ceac6813030dbe9c9e0d0742b3bac65c
rajat29 <rajat.sharma@nectechnologies.in>
2017-07-06 12:14:59 +0530

    Switch from oslosphinx to openstackdocstheme
    As part of the docs migration work for Pike we need to switch to use
    the openstackdocstheme.
    
    Change-Id: I73d80355f81bd68b0072d1879a3f21154cc6c8d7

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,10 +22,14 @@ sys.path.insert(0, os.path.abspath('../..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
-    'oslosphinx'
+    'openstackdocstheme',
     #'sphinx.ext.intersphinx',
 ]
 
+# openstackdocstheme options
+repository_name = 'openstack/congress'
+bug_project = 'congress'
+bug_tag = ''
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
 # text edit cycles.
 # execute "export SPHINX_DEBUG=1" in your terminal to disable
@@ -58,11 +62,13 @@ modindex_common_prefix = ['congress.']
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 # html_theme_path = ["."]
-# html_theme = '_theme'
+html_theme = 'openstackdocs'
 # html_static_path = ['static']
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = '%sdoc' % project
+
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass
